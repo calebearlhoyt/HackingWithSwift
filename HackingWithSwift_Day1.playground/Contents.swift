a1ddae2f88bd9f36271cf3158af152b456ba1a69
calebearlhoyt <32577278+calebearlhoyt@users.noreply.github.com>
2020-09-27 20:25:15 -0500

    Add files via upload

---
+++ HackingWithSwift_Day1.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+// Vars
+var str = "Hello, playground"
+
+str = "Goodbye"
+
+// Ints
+var age = 38
+
+// Multi Line Strings
+var str1 = """
+This goes over \
+multiple \
+lines
+"""
+
+// Doubles and Booleans
+
+var pi = 3.141
+
+var awesome = true
+
+// String interpolation
+var score = 85
+var strScore = "Your score was \(score)"
+
+// Constants
+let taylor = "swift"
+
+// Type Inference - i.e. a variable type must stay the same
